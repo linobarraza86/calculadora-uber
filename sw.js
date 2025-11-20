95273d6567b3321e1ebe40af992a2f8e83381698
linobarraza86 <pbf9h8rcxg@privaterelay.appleid.com>
2025-11-20 16:26:24 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("uber-calc").then(cache => {
-            return cache.addAll(["index.html", "manifest.json"]);
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("uber-calc-v1").then((cache) => {
+            return cache.addAll([
+                "index.html",
+                "manifest.json",
+                "icon.png"
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.map((key) => {
+                    if (key !== "uber-calc-v1") {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
-});
+
